7c117033bbc6fb2e9d2efb01397dab78d11bac19
Ron Gierlach <rongierlach@gmail.com>
2018-05-09 08:12:52 -0700

    Updates

--- path---claim-chip-with-metamask-a0e39f21c11f6a62c5ab.js
+++ path---claim-chip-with-metamask-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xc5cca3301077],{601:function(c,t){c.exports={pathContext:{}}}});
+webpackJsonp([0xc5cca3301077],{600:function(c,t){c.exports={pathContext:{}}}});
 //# sourceMappingURL=path---claim-chip-with-metamask-a0e39f21c11f6a62c5ab.js.map
--- path---team-a0e39f21c11f6a62c5ab.js
+++ path---team-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xb326e24804c6],{605:function(t,e){t.exports={pathContext:{}}}});
+webpackJsonp([0xb326e24804c6],{604:function(t,e){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---team-a0e39f21c11f6a62c5ab.js.map
